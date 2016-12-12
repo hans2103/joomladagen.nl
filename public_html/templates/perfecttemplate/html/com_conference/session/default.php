487e519d9f9a96cee2cd1b52f40cf75bec0a7428
Hans Kuijpers <hans2103@gmail.com>
2016-12-12 19:56:30 +0100

    #31 apply styling

--- public_html/templates/perfecttemplate/html/com_conference/session/default.php
+++ public_html/templates/perfecttemplate/html/com_conference/session/default.php
@@ -13,69 +13,72 @@ $this->loadHelper('format');
 $this->loadHelper('session');
 $speakers = array();
 
-if($this->item->conference_speaker_id)
+if ($this->item->conference_speaker_id)
 {
-	 $speakers = ConferenceHelperFormat::speakers($this->item->conference_speaker_id);
+	$speakers = ConferenceHelperFormat::speakers($this->item->conference_speaker_id);
 }
 ?>
 <div class="blog">
-	<div class="conference card">
-		<?php if($this->item->conference_speaker_id):?>
-			<?php foreach($speakers as $speaker) :?>
-				<div class="card__image">
+    <div class="conference card">
+		<?php if ($this->item->conference_speaker_id): ?>
+			<?php foreach ($speakers as $speaker) : ?>
+                <div class="card__image">
 					<?php echo JHtml::_('image', $speaker->image, $this->escape($speaker->title)); ?>
-					<?php if($speaker->enabled):?>
-
-						<a class="btn btn-small btn-block" href="index.php?option=com_conference&view=speaker&id=<?php echo $speaker->conference_speaker_id?>">
-							<span class="icon icon-user"></span> <?php echo(trim($speaker->title))?>
-						</a>
+					<?php if ($speaker->enabled): ?>
+						<?php echo JHtml::_('link', JRoute::_('index.php?option=com_conference&view=speaker&id=' . $speaker->conference_speaker_id), trim($speaker->title)); ?>
 					<?php else: ?>
-						<span class="btn btn-small btn-block disabled"><span class="icon icon-user"></span> <?php echo(trim($speaker->title))?></span>
+						<?php echo(trim($speaker->title)); ?>
 					<?php endif; ?>
-				</div>
+                </div>
 			<?php endforeach; ?>
 		<?php else: ?>
-			<div class="card__image">
-			  <img src="http://placehold.it/200x200">
-			</div>
+            <div class="card__image">
+                <img src="http://placehold.it/200x200">
+            </div>
 		<?php endif; ?>
 
-		<div class="card__content">
-			<div class="card__header">
-				<h1>
-					<?php echo $this->escape($this->item->title)?>
-				</h1>
-				<div class="card__actions speakersocial">
-					<?php if($this->item->conference_slot_id):?>
-						<a class="btn btn-small btn-block" href="<?php echo JRoute::_('index.php?option=com_conference&view=days')?>">
-							<span class="icon icon-clock"></span> <?php echo (ConferenceHelperSession::slot($this->item->conference_slot_id))?>
-						</a>
-					<?php endif;?>
-					<?php if($this->item->conference_level_id):?>
-						<a class="btn btn-small btn-block" href="<?php echo JRoute::_('index.php?option=com_conference&view=levels')?>">
-							<span class="icon icon-equalizer"></span> <?php echo (ConferenceHelperSession::level($this->item->conference_level_id)->title)?>
-						</a>
-					<?php endif;?>
-				</div>
-				<?php if($this->item->conference_room_id):?>
-					<span class="">
-								<span class="icon icon-home"></span> <?php echo (ConferenceHelperSession::room($this->item->conference_room_id)->title)?>
-							</span>
-				<?php endif;?>
-				<?php if((ConferenceHelperParams::getParam('language',0)) && ($this->item->language)):?>
-					<span class="">
-								<span class="icon icon-comments-2"></span> <?php echo (ConferenceHelperSession::language($this->item->language))?>
-							</span>
-				<?php endif;?>
-			</div>
+        <div class="card__content">
+            <div class="card__header">
+                <h1>
+					<?php echo $this->escape($this->item->title) ?>
+                </h1>
+                <div class="card__meta">
+					<?php
+					if ($this->item->conference_slot_id)
+					{
+						echo JHtml::_('link', JRoute::_('index.php?option=com_conference&view=days'), ConferenceHelperSession::slot($this->item->conference_slot_id), array('class' => 'card__meta-item card__meta-item--slot'));
+					}
 
-			<div class="card__body">
+					if ($this->item->conference_level_id)
+					{
+						echo JHtml::_('link', JRoute::_('index.php?option=com_conference&view=levels'), JText::_('COM_CONFERENCE_FIELD_LEVEL') . ": " . ConferenceHelperSession::level($this->item->conference_level_id)->title, array('class' => 'card__meta-item card__meta-item--level'));
+
+					}
+
+					if ($this->item->conference_room_id)
+					{
+						echo '<span class="card__meta-item card__meta-item--room">';
+						echo '  ' . ConferenceHelperSession::room($this->item->conference_room_id)->title;
+						echo '</span>';
+					}
+
+					if ((ConferenceHelperParams::getParam('language', 0)) && ($this->item->language))
+					{
+						echo '<span class="card__meta-item card__meta-item--language">';
+						echo '  ' . ConferenceHelperSession::language($this->item->language);
+						echo '</span>';
+					}
+					?>
+					</div >
+            </div >
+
+            <div class="card__body" >
 				<?php echo($this->item->description) ?>
-			</div>
+                </div>
 
-			<div class="card__presentations">
-				<?php echo ($this->item->slides)?>
-			</div>
-		</div>
-	</div>
-</div>
+                <div class="card__presentations">
+					<?php echo($this->item->slides) ?>
+                </div>
+            </div>
+        </div>
+    </div>
