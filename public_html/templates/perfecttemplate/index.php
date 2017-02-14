7bdaceaa0f793c21b10c4f1aaee897e78a67f2b4
Hans Kuijpers <hans2103@gmail.com>
2017-02-14 20:36:37 +0100

    #71 add countdown early bird

--- public_html/templates/perfecttemplate/index.php
+++ public_html/templates/perfecttemplate/index.php
@@ -47,6 +47,38 @@ require_once JPATH_THEMES . '/' . $this->template . '/html/layouts/perfectlayout
     </div>
 </header>
 
+<?php
+
+$date      = JFactory::getDate();
+$edate     = new DateTime('2017-02-21');
+$remain    = $edate->diff($date);
+$imgTitle  = '';
+$imgNumber = '';
+
+if ($remain->days >= 6 && $remain->days <= 13)
+{
+	$imgTitle  = 'weeks';
+	$imgNumber = ceil($remain->days / 7);
+}
+
+if ($remain->days >= 0 && $remain->days <= 5)
+{
+	$imgTitle  = 'days';
+	$imgNumber = $remain->days;
+}
+
+if (isset($imgTitle) && !empty($imgTitle))
+{ ?>
+    <div class="block__countdown">
+        <div class="block__countdown--container">
+            <div class="block__countdown--content">
+				<?php $img = JHtml::_('image', 'images/countdown/' . $imgTitle . '-' . $imgNumber . '.png', 'Geniet nu nog van de Early Bird'); ?>
+				<?php echo JHtml::_('link', 'https://shop.joomladagen.nl', $img, array()); ?>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+
 <?php if (PWTTemplateHelper::isHome() == true) : ?>
 	<?php if ($this->countModules('header')) : ?>
         <div class="block__paralax block__header--home">
