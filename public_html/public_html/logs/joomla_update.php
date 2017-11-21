9b8b7a3fd4198886cc1df828ddde09e14ca72579
Added through Byte server <hans2103@gmail.com>
2017-11-21 13:51:05 +0100

    housecleaning

--- public_html/public_html/logs/joomla_update.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2017-11-07 16:44:50 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2017-11-07T16:44:50+00:00	INFO 34.250.7.114	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
