e4df522bb43fdb4d9376b241bbc1af35e93513fe
Hans Kuijpers <hans2103@gmail.com>
2017-12-18 15:28:42 +0100

    styling for forms and j2store

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         },
         browsersync : {
             port : '5666', //NVML
-            proxy: 'joomladagen.dev',
+            proxy: 'joomladagen.test',
             open: false
         }
     };
