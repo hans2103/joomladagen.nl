3707f996205e20135fb86616397dccaab930384a
Hans Kuijpers <info@hkweb.nl>
2018-01-24 11:19:18 +0100

    #112 add JLayout published_date

---
+++ public_html/templates/jd18nl/html/layouts/template/content/publish_date.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Layout
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+$date   = $displayData['date'];
+$class  = isset($displayData['class']) ? $displayData['class'] : 'content';
+$format = JText::_($displayData['format']);
+
+echo '<span class="' . $class . '__publish">';
+echo '<time datetime="' . JHtml::_('date', $date, 'c') . '" itemprop = "datePublished" >';
+echo JHtml::_('date', $date, $format);
+echo '  </time>';
+echo '</span>';
