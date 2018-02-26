11c2bcbac912c126bc0b222a0ee3eebb29d9f1f6
Hans Kuijpers <info@hkweb.nl>
2018-02-26 11:15:33 +0100

    fix merge conflicts

--- public_html/templates/jd18nl/html/com_conference/session/edit.php
+++ public_html/templates/jd18nl/html/com_conference/session/edit.php
@@ -8,13 +8,9 @@
  * @link        https://stichtingsympathy.nl
  */
 
-<<<<<<< HEAD
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-=======
-use Joomla\CMS\HTML\HTMLHelper;
->>>>>>> master
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Uri\Uri;
@@ -39,8 +35,6 @@ JFactory::getDocument()->addScriptDeclaration("
             }
         }
 ");
-<<<<<<< HEAD
-
 
 $this->template = Factory::getApplication()->getTemplate();
 require_once JPATH_THEMES . '/' . $this->template . '/html/layouts/render.php';
@@ -110,56 +104,3 @@ echo JLayouts::render('template.content.header', $array);
         </div>
     </div>
 </section>
-=======
-?>
-<div class="conference">
-	<form name="sessionForm" id="sessionForm" class="form form-horizontal form-validate" action="<?php echo Route::_('index.php?option=com_conference&task=session.edit&id=' . $this->form->getValue('conference_session_id')); ?>" method="post" enctype="multipart/form-data">
-		<?php
-		echo  LayoutHelper::render('toolbar', array(
-			'title' => 'COM_CONFERENCE_FIELD_SESSION',
-			'view'  => 'session'
-		));
-		?>
-		<div class="well well-small">
-			<!-- Start row -->
-			<div class="row-fluid">
-				<!-- Start left -->
-				<div class="span12">
-					<?php echo $this->form->renderField('title'); ?>
-					<div class="control-group">
-						<label for="conference_level_id" class="control-label">
-							<?php echo $this->form->getLabel('conference_level_id'); ?>
-						</label>
-						<div class="controls">
-							<?php echo $this->form->getInput('conference_level_id'); ?>
-							<span class="help-block"><a href="<?php echo JRoute::_('index.php?option=com_conference&view=levels')?>"><?php echo JText::_('COM_CONFERENCE_FIELD_LEVEL_DESC'); ?></a></span>
-							
-						</div>
-					</div>
-					<?php if ($params->get('language', 0)): ?>
-						<?php echo $this->form->renderField('language'); ?>
-					<?php endif;?>
-					<hr>
-					<?php echo $this->form->renderField('description'); ?>
-					<hr>
-					<div class="control-group">
-						<label for="slides" class="control-label">
-							<?php echo $this->form->getLabel('slides'); ?>
-						</label>
-						<div class="controls">
-							<?php echo $this->form->getInput('slides'); ?>
-							<span class="help-block"><?php echo JText::_('COM_CONFERENCE_FIELD_SLIDES_DESC')?></span>
-						</div>
-					</div>
-				</div>
-				<!-- End left -->
-			</div>
-		</div>
-		<input type="hidden" name="task" value="" />
-		<input type="hidden" name="return" value="<?php echo $returnUrl; ?>" />
-		<?php echo $this->form->renderField('conference_session_id'); ?>
-		<?php echo $this->form->renderField('enabled'); ?>
-		<?php echo HTMLHelper::_('form.token'); ?>
-	</form>
-</div>
->>>>>>> master
