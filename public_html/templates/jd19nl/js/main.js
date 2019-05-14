d4cb9677f72749836ac73ab32975aacd5054dc70
Sander Potjer <github@sanderpotjer.nl>
2019-05-14 11:11:18 +0200

    Set correct counter time

--- public_html/templates/jd19nl/js/main.js
+++ public_html/templates/jd19nl/js/main.js
@@ -55,7 +55,7 @@ jQuery(function ($) {
 
     if ($('.countdown').length > 0) {
         $(".countdown").jCounter({
-            date: '17 May 2019 09:00:00',
+            date: '17 May 2019 09:15:00',
             fallback: function () {
                 console.log("count finished!")
             }
