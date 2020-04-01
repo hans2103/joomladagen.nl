e12ea998b54d572fb1227b2f938f7f1e88d2fa61
Sander Potjer <github@sanderpotjer.nl>
2020-04-01 22:50:25 +0200

    Move landing page to template

---
+++ public_html/templates/jd20nl/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+	<title>JoomlaDagen Nederland - 30 en 31 januari 2021 - Baarlo</title>
+	<meta name="description" content="Op zaterdag 30 en zondag 31 januari 2021 zal hét jaarlijkse event voor iedereen die websites bouwt met Joomla plaatsvinden in Baarlo.">
+	<meta name="robots" content="index, follow">
+	<meta name="author" content="Stichting Joomla Nederland">
+	<meta property="og:title" content="JoomlaDagen Nederland - 30 en 31 januari 2021 - Baarlo">
+	<meta property="og:site_name" content="JoomlaDagen 2021">
+	<meta property="og:url" content="https://www.joomladagen.nl">
+	<meta property="og:description" content="Op zaterdag 30 en zondag 31 januari 2021 zal hét jaarlijkse event voor iedereen die websites bouwt met Joomla plaatsvinden in Baarlo.">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="https://www.joomladagen.nl/images/joomladagen.jpg">
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@joomladagen">
+	<meta name="twitter:creator" content="@joomladagen">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,600">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800">
+
+	<style>
+		body, html {
+			height: 100%;
+		}
+
+		.bg {
+			background-image: url(images/pattern.png);
+			background-position: center;
+			background-repeat: repeat;
+			background-size: 35%;
+			background-attachment: fixed;
+		}
+
+		.jumbotron {
+			background: #f9fafc;
+			padding-bottom: 1em;
+			margin: 0;
+			max-width: 750px;
+		}
+
+		h1 {
+			font-family: 'Raleway', sans-serif;
+			font-weight: 800;
+			color: #25304f;
+			font-size: 2.4em;
+		}
+
+		p.lead {
+			font-size: 1.1em;
+			font-weight: 300;
+		}
+
+		p {
+			font-family: 'Roboto', sans-serif;
+			font-size: 1.05em;
+			font-weight: 300;
+		}
+
+		h3 {
+			font-family: 'Raleway', sans-serif;
+			font-size: 1.5em;
+			font-weight: 400;
+			color: #25304f;
+		}
+
+		strong {
+			font-weight: 600;
+		}
+	</style>
+	<script>
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start':
+					new Date().getTime(), event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-NQZ8SP');
+	</script>
+</head>
+
+<body class="bg">
+<noscript>
+	<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQZ8SP" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+
+<div class="container-fluid h-100">
+	<div class="row justify-content-center h-100">
+		<div class="col-sm-11 col-md-10 col-lg-9 col-xl-6 my-auto pt-3 pb-3">
+			<div class="jumbotron text-center mx-auto">
+				<h5>30 en 31 januari 2021 - Baarlo</h5>
+				<h1>JoomlaDagen Nederland</h1>
+				<p class="lead">
+					Pak je agenda erbij! En noteer de 15e editie van de Nederlandse JoomlaDagen.<br>Op zaterdag 30 en zondag 31 januari 2021 zal hét jaarlijkse event voor iedereen die websites bouwt met Joomla plaatsvinden in Baarlo, Limburg.
+				</p>
+				<p>
+					Momenteel werken we onze ideeën en plannen voor deze jubileumeditie uit.<br> Zodra er meer bekend is zal dat uiteraard op deze site te vinden zijn!
+				</p>
+				<p>
+					Ideeën, suggesties of wil je sponsoren?<br> Onze
+					<a href="mailto:info@joomladagen.nl">mailbox staat open</a>!
+				</p>
+				<p>
+					<em>De 15e editie stond gepland voor 27 en 28 juni 2020.<br>Vanwege de Coronacrisis hebben we besloten om deze te verplaatsen naar volgend jaar.</em>
+				</p>
+				<p>
+					<small>De JoomlaDagen worden georganiseerd door
+						<a href="https://www.stichtingjoomlanederland.nl/">Stichting Joomla Nederland</a>
+					</small>
+				</p>
+			</div>
+			<div class="col-sm-11 mx-auto" style="font-size:10px; color: #fff; text-align: center; margin-top:10px;">JoomlaDay™ events are officially recognized and licensed by, but not organized or operated by, Open Source Matters, Inc. (OSM) on behalf of The Joomla! Project™. Each JoomlaDay is managed independently by a local community. Use of the Joomla!® name, symbol, logo, JoomlaDay,™ and JDay™ and related trademarks is licensed by Open Source Matters, Inc.</div>
+		</div>
+	</div>
+</div>
+
+</body>
+
+</html>
